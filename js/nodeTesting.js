687a398c62904dc319977b1cbe673f553d115565
Daniel Dominguez <dannickfox@gmail.com>
2016-08-02 03:31:07 -0500

    test file

--- js/nodeTesting.js
+++ js/nodeTesting.js
@@ -7,12 +7,7 @@ const Environment = require('./phyzzy/components/environment.js')
 const Vector = (x, y) => new Vect(x, y)
 
 const ph = Phyzzy(100)
-const bounds = {
-    x: 0,
-    y: 0,
-    w: 5,
-    h: 5
-}
+const bounds = {x: 0, y: 0, w: 5, h: 5}
 const env = Environment({x: 0, y: -9.81}, 0.3, bounds)
 const m1 = Mass(0.5, 0.05, 0.8, 0.8, 0.6, {x: 5, y: 5})
 const m2 = Mass(0.5, 0.05, 0.8, 0.8, 0.6, {x: 5, y: 5})
