dfe96bf7c925ea2aa4814b611f313022cb499a8a
Daniel Dominguez <dannickfox@gmail.com>
2016-08-06 00:16:11 -0500

    added condition to prevent repititive drawing.

--- js/phyzzy/engine.js
+++ js/phyzzy/engine.js
@@ -34,22 +34,26 @@ const CanvasDraw = state => ({
         const traces = []
         state.m.forEach(mass => {
             mass.branch.forEach(b => {
-                ctx.beginPath()
-                ctx.moveTo (
-                    mass.Pi.x * state.scale,
-                    mass.Pi.y * state.scale
-                )
-                ctx.lineTo (
-                    b.m.Pi.x * state.scale,
-                    b.m.Pi.y * state.scale
-                )
-                ctx.strokeStyle = colorS || '#000000'
-                ctx.stroke()
-                ctx.closePath()
-                traces.push({m1: mass, m2: b.m})
+                if (!traces.find(
+                    t => b.m === t.m1 && mass === t.m2 || b.m === t.m2 && mass === t.m1)
+                ) {
+                    // mesh is non-linear, traces must be tracked to avoid repetition
+                    ctx.beginPath()
+                    ctx.moveTo (
+                        mass.Pi.x * state.scale,
+                        mass.Pi.y * state.scale
+                    )
+                    ctx.lineTo (
+                        b.m.Pi.x * state.scale,
+                        b.m.Pi.y * state.scale
+                    )
+                    ctx.strokeStyle = colorS || '#000000'
+                    ctx.stroke()
+                    ctx.closePath()
+                    traces.push({m1: mass, m2: b.m})
+                }
             })   
         })
-        console.log(traces)
     }
 })
 
