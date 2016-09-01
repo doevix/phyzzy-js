8b09114d446ceb867112b1d9ad5f1957f838c4ea
Daniel Dominguez <dannickfox@gmail.com>
2016-09-01 18:25:53 -0500

    bounds

--- phMain.js
+++ phMain.js
@@ -28,50 +28,21 @@ let delta = 1 / 50 // step time
 const ph = Phyzzy(100)
 
 const env = Environment(
-    {x: 0, y: 0},
-    20.0,
-    {x: 0.1, y: 0.1, w: viewport.width / ph.scale - 0.1, h: viewport.height / ph.scale - 0.1}
+    {x: 0, y: 9.81},
+    0.0,
+    {x: 0, y: 0, w: viewport.width / ph.scale, h: viewport.height / ph.scale}
 )
 
 const mouse = User.Mouser(ph.scale)
 
 mouse.init(viewport, ph)
 
-const mProp = {mass: 0.5, rad: 0.05, refl: 0.75, mu_s: 0.5, mu_k: 0.4, q: 5}
-const mProp2 = {mass: 0.5, rad: 0.05, refl: 0.75, mu_s: 0.0, mu_k: 0.4, q: -1}
-const mProp3 = {mass: 0.5, rad: 0.05, refl: 0.75, mu_s: 0.0, mu_k: 0.4, q: -1}
-// Builders.generateBox(1, 1, 2, 2, mProp2, 100, 50, ph)
-// Builders.generateBox(2.5, 2.5, 3, 3, mProp, 100, 50, ph)
-// Builders.generateTriangle(2, 2, 1, -Math.sqrt(3) * 1 / 2, mProp, 100, 50, ph)
+const mProp = {mass: 0.1, rad: 0.05, refl: 0.75, mu_s: 0.5, mu_k: 0.4}
 
-const genRand = (x, y, w, h) => ({x: x + Math.random() * w, y: y + Math.random() * h})
-
-for (let i = 0; i < 200; i++) {
-    let posi = genRand(0, 0, env.boundary.x + env.boundary.w, env.boundary.y + env.boundary.h)
-    let poso = genRand(0, 0, 5, 5)
-    ph.addM(
-        Mass(
-            {mass: 0.5, rad: 0.05, refl: 0.75, mu_s: 0.5, mu_k: 0.4, q: 1},
-            posi, posi
-        )
-    )
-}
-
-// const boundCenter = {x: (env.boundary.x + env.boundary.w) / 2, y: (env.boundary.y + env.boundary.h) / 2}
-
-// ph.addM(Mass(mProp3, boundCenter, boundCenter))
-// ph.addM(Mass(mProp, {x: boundCenter.x - 500, y: boundCenter.y}, {x: boundCenter.x - 500, y: boundCenter.y - 40}))
-// ph.addM(Mass(mProp2, {x: boundCenter.x - 520, y: boundCenter.y}, {x: boundCenter.x - 520, y: boundCenter.y - 40}))
-// ph.addM(Mass(mProp2, {x: boundCenter.x - 520, y: boundCenter.y}, {x: boundCenter.x - 540, y: boundCenter.y - 30}))
+Builders.generateBox(1, 1, 2, 2, mProp, 100, 50, ph)
 
 const frame = (frameTime) => {
     ctx.clearRect(0, 0, viewport.width, viewport.height)
-    ctx.strokeRect(
-        env.boundary.x * ph.scale,
-        env.boundary.y * ph.scale,
-        (env.boundary.w - env.boundary.x) * ph.scale,
-        (env.boundary.h - env.boundary.y) * ph.scale
-    )
     ph.drawSpring(ctx, '#000000')
     ph.drawMass(ctx, '#1DB322')
     mouse.hover(ph, ctx, '#1DB322')
@@ -80,16 +51,11 @@ const frame = (frameTime) => {
         ph.verlet(ph.mesh.map(mass => {
             let f = env.weight(mass).sum(env.drag(mass))
             .sum(mass.springing()).sum(mass.damping())
-            .sum(extraForces.Coulomb(mass, ph.mesh, 0.05))
-            // f = f.sum(env.friction(mass, f))
-            f.sumTo(env.squishyBounds(mass, f, 50, 10))
+            f = f.sum(env.friction(mass, f))
             f = mass !== mouse.dragging() ? f : f.mul(0)
-            // the following are drawing functions for visualizing F and V
-            // f.canvasDraw(mass.Pi, ph.scale, 0.1, ctx, '#FF5555')
-            // mass.vel(delta).canvasDraw(mass.Pi, ph.scale, 0.1, ctx, '#7777FF')
             return f
         }), delta)
-        // ph.collision(ph.mesh.map(mass => env.boundaryHit(mass)))
+        ph.collision(ph.mesh.map(mass => env.boundaryHit(mass)))
     }
 
     mouse.dragMass(pause)
