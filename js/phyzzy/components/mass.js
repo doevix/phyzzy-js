876384645114740db753138f7fdf232d8f44ea80
Daniel Dominguez <dannickfox@gmail.com>
2016-09-09 13:26:09 -0500

    faster springing and damping calculations

--- js/phyzzy/components/mass.js
+++ js/phyzzy/components/mass.js
@@ -13,23 +13,45 @@ const Springing = state => ({
     springing: () => state.branch.reduce((lf, cf) => lf.sum(cf.s.springing(state.Pi, cf.m.Pi)), new Vect())
 })
 
+const FastSpringing = state => ({
+    // quicker implementation of springing force calculation
+    springing: () => {
+        const force = new Vect()
+        for (let leaf of state.branch) {
+            force.sumTo(leaf.s.springing(state.Pi, leaf.m.Pi))
+        }
+        return force
+    }
+})
+
 const Damping = state => ({
     // sum forces acted on mass by dampers
-    damping: () => state.branch.reduce((lf, cf) => lf.sum(cf.s.damping(state.Pi, state.Po, cf.m.Pi, cf.m.Po)), new Vect())
+    damping: () => state.branch.reduce((lf, cf) => lf.sum(cf.s.damping(state, cf.m)), new Vect())
+})
+
+const FastDamping = state => ({
+    // quicker implementation of damping force calculation
+    damping: () => {
+        const force = new Vect()
+        for (let leaf of state.branch) {
+            force.sumTo(leaf.s.damping(state, leaf.m))
+        }
+        return force
+    }
 })
 
 const Mass = (prop, Pi, Po) => {
     let state = {
         Pi: new Vect(Pi.x, Pi.y),
-        Po: new Vect(Po.x, Po.y),
+        Po: Po ? new Vect(Po.x, Po.y) : new Vect(Pi.x, Pi.y),
         branch: []
     }
-    Object.assign(state, prop)
     return Object.assign(
         state,
+        prop,
         Velocity(state),
-        Springing(state),
-        Damping(state)
+        FastSpringing(state),
+        FastDamping(state)
     )
 }
 
