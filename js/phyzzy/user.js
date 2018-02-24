5df0a87d6011335e23e82a332f3e2153f669475d
Daniel Dominguez <dannickfox@gmail.com>
2018-02-24 02:04:37 -0600

    format

--- js/phyzzy/user.js
+++ js/phyzzy/user.js
@@ -1,111 +1,111 @@
 // user.js
 // mousing functions for user interaction
-'use strict'
-const Vect = require('./components/vector.js')
-const highlightRadius = 5
-const padding = 10
+
+const Vect = require('./components/vector.js');
+const highlightRadius = 5;
+const padding = 10;
 
 const Highlighter = state => ({
     hover: (phyzzy, ctx, hColor) => {
-        ctx.strokeStyle = hColor || '#000000'
+        ctx.strokeStyle = hColor || '#000000';
         
         if (state.hov && state.sel !== state.hov) {
-            ctx.beginPath()
+            ctx.beginPath();
             ctx.arc(
                 state.hov.Pi.x * state.scale,
                 state.hov.Pi.y * state.scale,
                 state.hov.rad * state.scale + highlightRadius,
                 0, 2 * Math.PI
-            )
-            ctx.stroke()
-            ctx.closePath()
+            );
+            ctx.stroke();
+            ctx.closePath();
         }
-        return state.hov
+        return state.hov;
     },
     select: (ctx, sColor) => {
-        ctx.strokeStyle = sColor || '#000000'
+        ctx.strokeStyle = sColor || '#000000';
 
         if (state.sel) {
-            ctx.beginPath()
+            ctx.beginPath();
             ctx.arc(
                 state.sel.Pi.x * state.scale,
                 state.sel.Pi.y * state.scale,
                 state.sel.rad * state.scale + highlightRadius,
                 0, 2 * Math.PI, false
-            )
-            ctx.stroke()
-            ctx.closePath()
+            );
+            ctx.stroke();
+            ctx.closePath();
         }
-        return state.sel
+        return state.sel;
     } 
 })
 
 const Mover = state => ({
     dragMass: isPaused => {
         if (state.sel && state.mousedown && state.hov === state.sel || state.drg) {
-            if (!state.drg) state.drg = state.sel 
-            state.sel.Pi.equ(state.Pi.div(state.scale))
+            if (!state.drg) state.drg = state.sel; 
+            state.sel.Pi.equ(state.Pi.div(state.scale));
             // prevents unwanted speed changes after getting out of pause mode.
-            if (isPaused || state.drg.fixed) state.sel.Po.equ(state.Pi.div(state.scale))
+            if (isPaused || state.drg.fixed) state.sel.Po.equ(state.Pi.div(state.scale));
             
         }
     }
-})
+});
 
 const Initializer = state => ({
     init: (canvas, phyzzy) => {
 
         canvas.onmousedown = e => {
             // actions when mouse has been clicked
-            if (!state.mousedown) state.mousedown = true
+            if (!state.mousedown) state.mousedown = true;
 
             const isOnHov = state.hov ? state.hov.Pi.compare(
                 state.Pi.div(state.scale), state.hov.rad + padding / state.scale
-            ) : false
+            ) : false;
 
             if (isOnHov && state.sel !== state.hov) {
-                state.sel = state.hov
+                state.sel = state.hov;
             } else {
-                state.sel = undefined
-                state.hov = undefined
+                state.sel = undefined;
+                state.hov = undefined;
             }
         }
         canvas.onmouseup = e => {
             // actions when mouse stops clicking
-            if (state.mousedown) state.mousedown = false
-            if (state.drg) state.drg = undefined
+            if (state.mousedown) state.mousedown = false;
+            if (state.drg) state.drg = undefined;
         }
 
         canvas.onmouseenter = e => {
-            state.drg = undefined
-            state.mousedown = false
+            state.drg = undefined;
+            state.mousedown = false;
         }
 
         canvas.onmousemove = e => {
             // actions when mouse moves in the canvas
-            const b = canvas.getBoundingClientRect()
-            state.Po.equ(state.Pi)
+            const b = canvas.getBoundingClientRect();
+            state.Po.equ(state.Pi);
             state.Pi.equ({
                 x: e.clientX - b.left,
                 y: e.clientY - b.top
-            })
+            });
 
             state.hov = phyzzy.mesh.find(
                 m => m.Pi.compare(
                     state.Pi.div(state.scale),
                     m.rad + padding / state.scale
                 )
-            )
+            );
         }
 
     }
-})
+});
 
 const Output = state => ({
     coord: scale => state.Pi.div(scale || state.scale),
     isDown: () => state.mousedown,
     dragging: () => state.drg
-})
+});
 
 const Mouser = scale => {
     const state = {
@@ -116,14 +116,14 @@ const Mouser = scale => {
         hov: undefined,
         sel: undefined,
         drg: undefined
-    }
+    };
     return Object.assign(
         {},
         Initializer(state),
         Highlighter(state),
         Mover(state),
         Output(state)
-    )
+    );
 }
 
 module.exports = {
