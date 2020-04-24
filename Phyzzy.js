81253168ff0a6db4f15d88eb4a6cba04816b6465
Laura Fox <dannickfox@gmail.com>
2020-04-24 00:57:58 -0500

    changed mouse input

--- Phyzzy.js
+++ Phyzzy.js
@@ -6,6 +6,11 @@ class Vect {
         this.x = x || 0;
         this.y = y || 0;
     }
+    set(x, y)
+    {
+        this.x = x;
+        this.y = y;
+    }
     // replace values of vector with given vector
     equ(A) {
         this.x = A.x;
@@ -77,8 +82,8 @@ class Vect {
         return V.x === this.x && V.y === this.y;
     }
     // compares if a coordinate is within the bounds of another according to a boundary radius
-    compare(A, rad) {
-        return Math.abs(this.x - A.x) <= rad && Math.abs(this.y - A.y) <= rad;
+    isNear(A, rad) {
+        return Math.abs(this.x - A.x) < rad && Math.abs(this.y - A.y) < rad;
     }
     // returns a string that displays the vector's components
     display (fix) {
@@ -127,9 +132,11 @@ class Mass {
         this.refl = prop.refl;
         this.mu_s = prop.mu_s;
         this.mu_k = prop.mu_k;
+        this.fix = false;
+        this.ignore = false;
     }
     vel(delta) {
-        this.Pi.sub(this.Po).div(delta);
+        return this.Pi.sub(this.Po).div(delta);
     }
     springing() {
         const force = new Vect();
@@ -145,6 +152,17 @@ class Mass {
         }
         return force;
     }
+    moveVerlet(forces, delta) {
+        if (!this.fix && !this.ignore) {
+            let accel = forces.div(this.mass);
+            let delta_Pi = this.Pi.sub(this.Po).sum(accel.mul(delta * delta));
+            this.Po.equ(this.Pi);
+            this.Pi.sumTo(delta_Pi);
+        } else if (this.fix)
+        {
+            this.Po.equ(this.Pi);
+        }
+    }
 };
 
 class PhyzzyModel {
@@ -152,6 +170,7 @@ class PhyzzyModel {
     {
         this.scale = scale;
         this.mesh = [];
+        this.springs = [];
     }
     addM(mass) {
         if (!this.mesh.some(m => m === mass)) {
@@ -165,6 +184,7 @@ class PhyzzyModel {
             // cannot link a mass to itself nor have two springs in link
             mass1.branch.push({m: mass2, s: spring});
             mass2.branch.push({m: mass1, s: spring});
+            this.springs.push(spring);
         }
     }
     remM(mass) {
@@ -174,13 +194,20 @@ class PhyzzyModel {
     remS(spring) {
         this.mesh.forEach(mass => mass.branch = mass.branch.filter(leaf => leaf.s !== spring));
     }
-    verlet(forces, dt) {
+    locateMass(pos, rad)
+    {
+        return this.mesh.find(mass => mass.Pi.isNear(pos, rad));
+    }
+    update(forces, dt) {
         for (let i = 0; i < this.mesh.length; i++) {
             let mass = this.mesh[i];
-            let accel = forces[i].div(mass.mass);
-            let delta_Pi = mass.Pi.sub(mass.Po).sum(accel.mul(dt * dt));
-            mass.Po.equ(mass.Pi);
-            mass.Pi.sumTo(delta_Pi);
+            mass.moveVerlet(forces[i], dt);
+            // if (!mass.fix && !mass.ignore) {
+            //     let accel = forces[i].div(mass.mass);
+            //     let delta_Pi = mass.Pi.sub(mass.Po).sum(accel.mul(dt * dt));
+            //     mass.Po.equ(mass.Pi);
+            //     mass.Pi.sumTo(delta_Pi);
+            // }
         }        
     }
     collision(collCoord) {
@@ -292,292 +319,103 @@ class PhyzzyEnvironment {
         return friction;
     }
 }
-const highlightRadius = 5;
-const padding = 10;
-class Mouser
-{
-    constructor(scale)
-    {
-        this.Pi = new Vect(0, 0);
-        this.Po = new Vect(0, 0);
-        this.scale = scale;
-        this.mousedown = false;
-        this.hov = undefined;
-        this.sel = undefined;
-        this.drg = undefined;
-    }
-    init(canvas, phyzzy) {
-        canvas.onmousedown = e => {
-            // actions when mouse has been clicked
-            if (!this.mousedown) this.mousedown = true;
-
-            const isOnHov = this.hov ? this.hov.Pi.compare(
-                this.Pi.div(this.scale), this.hov.rad + padding / this.scale
-            ) : false;
-
-            if (isOnHov && this.sel !== this.hov) {
-                this.sel = this.hov;
-            } else {
-                this.sel = undefined;
-                this.hov = undefined;
-            }
-        }
-        canvas.onmouseup = e => {
-            // actions when mouse stops clicking
-            if (this.mousedown) this.mousedown = false;
-            if (this.drg) this.drg = undefined;
-        }
-
-        canvas.onmouseenter = e => {
-            this.drg = undefined;
-            this.mousedown = false;
-        }
-
-        canvas.onmousemove = e => {
-            // actions when mouse moves in the canvas
-            const b = canvas.getBoundingClientRect();
-            this.Po.equ(this.Pi);
-            this.Pi.equ({
-                x: e.clientX - b.left,
-                y: e.clientY - b.top
-            });
-
-            this.hov = phyzzy.mesh.find(
-                m => m.Pi.compare(
-                    this.Pi.div(this.scale),
-                    m.rad + padding / this.scale
-                )
-            );
-        }
-
-    }
-    hover(phyzzy, ctx, hColor) {
-        ctx.strokeStyle = hColor || '#000000';
-        
-        if (this.hov && this.sel !== this.hov) {
-            ctx.beginPath();
-            ctx.arc(
-                this.hov.Pi.x * this.scale,
-                this.hov.Pi.y * this.scale,
-                this.hov.rad * this.scale + highlightRadius,
-                0, 2 * Math.PI
-            );
-            ctx.stroke();
-            ctx.closePath();
-        }
-        return this.hov;
-    }
-    select(ctx, sColor) {
-        ctx.strokeStyle = sColor || '#000000';
-
-        if (this.sel) {
-            ctx.beginPath();
-            ctx.arc(
-                this.sel.Pi.x * this.scale,
-                this.sel.Pi.y * this.scale,
-                this.sel.rad * this.scale + highlightRadius,
-                0, 2 * Math.PI, false
-            );
-            ctx.stroke();
-            ctx.closePath();
-        }
-        return this.sel;
-    }
-    dragMass(isPaused) {
-        if (this.sel && this.mousedown && this.hov === this.sel || this.drg) {
-            if (!this.drg) this.drg = this.sel; 
-            this.sel.Pi.equ(this.Pi.div(this.scale));
-            // prevents unwanted speed changes after getting out of pause mode.
-            if (isPaused || this.drg.fixed) this.sel.Po.equ(this.Pi.div(this.scale));
-            
-        }
-    }
-    coord(scale){
-        return this.Pi.div(scale || this.scale);
-    }
-    isDown() {
-        return this.mousedown;
-    }
-    dragging() {
-        return this.drg;
-    }
-};
 
 // Special forces that can be added to the mapped force calculation.
-const Coulomb = (mass1, mesh, Kc) => {
-    // calculates electrostatic force via Coulombs law. (worst case: O(n^2))
-    if (mass1.q) {
-        return mesh.reduce((cSum, mass2) => {
+const SpecialForces = {
+    Coulomb: (mass1, mesh, Kc) => {
+        // calculates electrostatic force via Coulombs law. (NOTE: O(n^2))
+        if (mass1.q) {
+            return mesh.reduce((cSum, mass2) => {
+                if (mass1 !== mass2 && !mass1.Pi.compare(mass2.Pi, mass1.rad)) {
+                    let r = mass1.Pi.sub(mass2.Pi)
+                    return cSum.sum(r.unit().mul(Kc * mass1.q * mass2.q / r.magSq()))
+                } else return cSum
+            }, new Vect())
+        } else return new Vect()
+    },
+    Gravitation: (mass1, mesh, Kg) => {
+        // calculates gravitational force via Newton's Law.
+        // This is O(n^2) make sure mesh has less than 1000 masses for this.
+        return mesh.reduce((gSum, mass2) => {
             if (mass1 !== mass2 && !mass1.Pi.compare(mass2.Pi, mass1.rad)) {
                 let r = mass1.Pi.sub(mass2.Pi)
-                return cSum.sum(r.unit().mul(Kc * mass1.q * mass2.q / r.magSq()))
-            } else return cSum
+                return gSum.sum(r.unit().mul(-Kg * mass1.mass * mass2.mass / r.magSq()))
+            } else return gSum
         }, new Vect())
-    } else return new Vect()
-}
-const Gravitation = (mass1, mesh, Kg) => {
-    // calculates gravitational force via Newton's Law.
-    // This is O(n^2) make sure mesh has less than 1000 masses for this.
-    return mesh.reduce((gSum, mass2) => {
-        if (mass1 !== mass2 && !mass1.Pi.compare(mass2.Pi, mass1.rad)) {
-            let r = mass1.Pi.sub(mass2.Pi)
-            return gSum.sum(r.unit().mul(-Kg * mass1.mass * mass2.mass / r.magSq()))
-        } else return gSum
-    }, new Vect())
-}
-const Brownian = (factor) => {
-    const signedRand = () => 2 * (Math.random() - 0.5) // random value from -1 to 1
-    return new Vect(factor * signedRand(), factor * signedRand())
-}
-const SquishyBounds = (boundary, mass, factorS) => {
-    // experimental spring-like boundary
-    let force = new Vect(0, 0)
+    },
+    Brownian: (factor) => {
+        const signedRand = () => 2 * (Math.random() - 0.5) // random value from -1 to 1
+        return new Vect(factor * signedRand(), factor * signedRand())
+    },
+    SquishyBounds: (boundary, mass, factorS) => {
+        // experimental spring-like boundary
+        let force = new Vect(0, 0)
 
-    const fCalc = (xi, xo, bound) => factorS * (bound - xi) - (xi - xo) * (factorS - factorS * mass.refl) / factorS
+        const fCalc = (xi, xo, bound) => factorS * (bound - xi) - (xi - xo) * (factorS - factorS * mass.refl) / factorS
 
-    if (mass.Pi.y > boundary.h - mass.rad) {
-        // h boundary hit
-        force.y = fCalc(mass.Pi.y, mass.Po.y, boundary.h - mass.rad)
-    } else if (mass.Pi.y < boundary.y + mass.rad) {
-        // y boundary hit
-        force.y = fCalc(mass.Pi.y, mass.Po.y, boundary.y + mass.rad)
+        if (mass.Pi.y > boundary.h - mass.rad) {
+            // h boundary hit
+            force.y = fCalc(mass.Pi.y, mass.Po.y, boundary.h - mass.rad)
+        } else if (mass.Pi.y < boundary.y + mass.rad) {
+            // y boundary hit
+            force.y = fCalc(mass.Pi.y, mass.Po.y, boundary.y + mass.rad)
+        }
+        if (mass.Pi.x > boundary.w - mass.rad) {
+            // w boundary hit
+            force.x = fCalc(mass.Pi.x, mass.Po.x, boundary.w - mass.rad)
+        } else if (mass.Pi.x < boundary.x + mass.rad) {
+            // x boundary hit
+            force.x = fCalc(mass.Pi.x, mass.Po.x, boundary.x + mass.rad)
+        }
+        return force;
     }
-    if (mass.Pi.x > boundary.w - mass.rad) {
-        // w boundary hit
-        force.x = fCalc(mass.Pi.x, mass.Po.x, boundary.w - mass.rad)
-    } else if (mass.Pi.x < boundary.x + mass.rad) {
-        // x boundary hit
-        force.x = fCalc(mass.Pi.x, mass.Po.x, boundary.x + mass.rad)
-    }
-    return force;
 }
 
 // generates shapes and adds them to the mesh.
-const FullLinkCreate = (vertices, property, spr, damp, engine) => {
-    const masses = vertices.map(vertex => new Mass(property, vertex, vertex))
-    masses.forEach(mass => engine.addM(mass))
-    engine.mesh.forEach(mass => {
-        engine.mesh.forEach(otherM => {
-            if (otherM !== mass && masses.find(m => m === mass) && masses.find(m => m === otherM)) {
-                engine.addS(mass, otherM, new Spring(mass.Pi.sub(otherM.Pi).mag(), spr, damp))
-            }
+const Builders = {
+    FullLinkCreate: function(vertices, property, spr, damp, engine) {
+        const masses = vertices.map(vertex => new Mass(property, vertex, vertex))
+        masses.forEach(mass => engine.addM(mass))
+        engine.mesh.forEach(mass => {
+            engine.mesh.forEach(otherM => {
+                if (otherM !== mass && masses.find(m => m === mass) && masses.find(m => m === otherM)) {
+                    engine.addS(mass, otherM, new Spring(mass.Pi.sub(otherM.Pi).mag(), spr, damp))
+                }
+            })
         })
-    })
-}
-const generateLine = (coordA, coordB, prop, spr, damp, eng) => {
-    let segAB = {x: coordA.x - coordB.x, y: coordA.y - coordB.y}
-    let restlength = Math.sqrt(segAB.x * segAB.x + segAB.y * segAB.y)
-    let m1 = new Mass(prop, coordA) 
-    let m2 = new Mass(prop, coordB) 
-    eng.addM(m1)
-    eng.addM(m2)
-    eng.addS(m1, m2, new Spring(restlength, spr, damp))
-}
-const generateBox = (x, y, w, h, prop, spr, damp, eng) => {
-    const vertices = [
-        {x: x, y: y},
-        {x: x + w, y: y},
-        {x: x + w, y: y + h},
-        {x: x, y: y + h}
-    ]
-    FullLinkCreate(vertices, Object.assign({}, prop), spr, damp, eng)
-}
-const generateTriangle = (x, y, b, h, prop, spr, damp, eng) => {
-    const vertices = [
-        {x: x, y: y},
-        {x: x + b, y: y},
-        {x: x + b / 2, y: y + h}
-    ]
-    FullLinkCreate(vertices, Object.assign({}, prop), spr, damp, eng)
-}
-const generateBlob = (x, y, w, h, N, prop, spr, damp, eng) => {
-    const randCoord = (p, l) => Math.random() * (l - p) + p
-    const vertices = []
-    for (let i = 0; i < N; i++) {
-        vertices.push({x: randCoord(x, w), y: randCoord(y, h)})
+    },
+    generateLine: function(coordA, coordB, prop, spr, damp, eng) {
+        let segAB = {x: coordA.x - coordB.x, y: coordA.y - coordB.y}
+        let restlength = Math.sqrt(segAB.x * segAB.x + segAB.y * segAB.y)
+        let m1 = new Mass(prop, coordA) 
+        let m2 = new Mass(prop, coordB) 
+        eng.addM(m1)
+        eng.addM(m2)
+        eng.addS(m1, m2, new Spring(restlength, spr, damp))
+    },
+    generateBox: function(x, y, w, h, prop, spr, damp, eng) {
+        const vertices = [
+            {x: x, y: y},
+            {x: x + w, y: y},
+            {x: x + w, y: y + h},
+            {x: x, y: y + h}
+        ]
+        this.FullLinkCreate(vertices, Object.assign({}, prop), spr, damp, eng)
+    },
+    generateTriangle: function(x, y, b, h, prop, spr, damp, eng) {
+        const vertices = [
+            {x: x, y: y},
+            {x: x + b, y: y},
+            {x: x + b / 2, y: y + h}
+        ]
+        this.FullLinkCreate(vertices, Object.assign({}, prop), spr, damp, eng)
+    },
+    generateBlob: function(x, y, w, h, N, prop, spr, damp, eng) {
+        const randCoord = (p, l) => Math.random() * (l - p) + p
+        const vertices = []
+        for (let i = 0; i < N; i++) {
+            vertices.push({x: randCoord(x, w), y: randCoord(y, h)})
+        }
+        this.FullLinkCreate(vertices, Object.assign({}, prop), spr, damp, eng)
     }
-    FullLinkCreate(vertices, Object.assign({}, prop), spr, damp, eng)
-}
-
-/**
- * Main program starts here
-*/
-
-const viewport = document.getElementById('viewport')
-const ctx = viewport.getContext('2d')
-const pauseButton = document.getElementById('userPause')
-let pause = pauseButton.value === 'pause' ? false : true
-pauseButton.addEventListener('click', e => {
-    if (!pause) {
-        pause = true
-        pauseButton.value = 'play'
-    } else {
-        pause = false
-        pauseButton.value = 'pause'
-    }
-}, false)
-
-
-let delta = 1 / 50 // step time
-const ph = new PhyzzyModel(100)
-
-const env = new PhyzzyEnvironment(
-    {x: 0, y: 9.81},
-    1,
-    {x: 0, y: 0, w: viewport.width / ph.scale, h: viewport.height / ph.scale}
-)
-
-const mouse = new Mouser(ph.scale)
-
-mouse.init(viewport, ph)
-
-const mPropA = {mass: 0.1, rad: 0.05, refl: 0.7, mu_s: 0.4, mu_k: 0.2}
-const mPropB = {mass: 0.5, rad: 0.05, refl: 0.7, mu_s: 0.4, mu_k: 0.2}
-
-generateBox(1, 1, 1, 1, mPropA, 100, 50, ph)
-generateBox(2, 2, 1, 1, mPropA, 100, 50, ph)
-generateBox(3.5, 4.5, 1, 1, mPropB, 500, 250, ph)
-generateBox(0.05, 4.5, 1, 1, mPropB, 500, 250, ph)
-
-generateLine({x: 1, y: 1}, {x: 1.5, y: 1.5}, mPropA, 100, 50, ph)
-
-
-
-ph.mesh[16].fixed = true
-
-for (let i = 0; i < 20; i++) {
-    let m = new Mass(mPropA,
-        {x: Math.random() * env.boundary.w, y: Math.random() * env.boundary.h},
-        {x: Math.random() * env.boundary.w, y: Math.random() * env.boundary.h}
-    )
-    ph.addM(m)
-}
-
-const frame = (frameTime) => {
-    ctx.clearRect(0, 0, viewport.width, viewport.height);
-    ph.drawSpring(ctx, '#000000');
-    ph.drawMass(ctx, '#1DB322');
-    mouse.hover(ph, ctx, '#1DB322');
-    mouse.select(ctx);
-    if (!pause){
-        ph.verlet(ph.mesh.map(mass => {
-            let f = env.weight(mass).sum(env.drag(mass))
-            .sum(mass.springing()).sum(mass.damping())
-            f = f.sum(env.friction(mass, f))
-            f = mass !== mouse.dragging() ? f : f.mul(0)
-            if (mass.fixed) f.clr();
-            return f
-        }), delta)
-        ph.collision(ph.mesh.map(mass => env.boundaryHit(mass)))
-    }
-
-    mouse.dragMass(pause)
-
-    ctx.fillStyle = '#000000'
-    ctx.fillText(mouse.coord().display(2), 20, 20)
-    ctx.fillText('mousedown ' + mouse.isDown(), 20, 30)
-
-    window.requestAnimationFrame(frame)
-}
-
-frame();
+};
---
+++ PhyzzyConstructor.js
@@ -0,0 +1,150 @@
+// PhyzzyConstructor.js
+
+// Initialize canvas element.
+const viewport = document.getElementById("viewport");
+const ctx = viewport.getContext("2d");
+
+// Initialize buttons.
+const pauseButton = document.getElementById("userPause");
+const constructButton = document.getElementById("userConstruct");
+const deleteButton = document.getElementById("userDelete");
+let pause = pauseButton.value === "pause" ? false : true;
+let construct = pauseButton.value === "construct" ? false : true;
+let udelete = pauseButton.value === "delete" ? false : true;
+pauseButton.addEventListener('click', e => {
+    if (!pause) {
+        pause = true;
+        pauseButton.value = 'play';
+    } else {
+        pause = false;
+        pauseButton.value = 'pause';
+    }
+}, false);
+constructButton.addEventListener('click', e => {
+    if (!construct) {
+        construct = true;
+        constructButton.value = 'construct';
+    } else {
+        construct = false;
+        constructButton.value = 'move/select';
+    }
+}, false);
+deleteButton.addEventListener('click', e => {
+    if (!udelete) {
+        udelete = true;
+        deleteButton.value = 'delete';
+    } else {
+        udelete = false;
+        deleteButton.value = 'select';
+    }
+}, false);
+
+// Initialize model.
+let delta = 1 / 50; // step time
+const ph = new PhyzzyModel(100);
+
+// Initialize environment.
+const env = new PhyzzyEnvironment(
+    {x: 0, y: 9.81},
+    1,
+    {x: 0, y: 0, w: viewport.width / ph.scale, h: viewport.height / ph.scale}
+);
+
+// Initialize user events.
+const userState = {
+    mousePos: new Vect(),
+    highlight: undefined,
+    select: undefined,
+    drag: undefined,
+    drawHighlight: function() {
+        if (this.highlight) {
+            const x = this.highlight.Pi.x * ph.scale;
+            const y = this.highlight.Pi.y * ph.scale;
+            const r = this.highlight.rad * ph.scale;
+            ctx.strokeStyle = "grey";
+            ctx.beginPath();
+            ctx.arc(x, y, r + r, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    },
+    drawSelect: function() {
+        if (this.select)
+        {
+            const x = this.select.Pi.x * ph.scale;
+            const y = this.select.Pi.y * ph.scale;
+            const r = this.select.rad * ph.scale;
+            ctx.strokeStyle = "black";
+            ctx.beginPath();
+            ctx.arc(x, y, r + r, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
+};
+viewport.addEventListener("mousemove", e => {
+    const b = viewport.getBoundingClientRect();
+    userState.mousePos.set(e.clientX - b.left , e.clientY - b.top);
+    userState.highlight = ph.locateMass(userState.mousePos.div(ph.scale), 0.15);
+    if (userState.drag) {
+        const mMovement = new Vect(e.movementX / ph.scale, e.movementY / ph.scale);
+        userState.drag.ignore = true;
+        userState.drag.Po.equ(userState.drag.Pi);
+        userState.drag.Pi.sumTo(mMovement);
+
+    }
+});
+viewport.addEventListener("mousedown", e => {
+    userState.select = ph.locateMass(userState.mousePos.div(ph.scale), 0.15);
+    userState.drag = userState.select;
+});
+viewport.addEventListener("mouseup", e => {
+    if (userState.drag) {
+        userState.drag.ignore = false;
+        userState.drag = undefined;
+    }
+});
+
+// Construct model.
+const mPropA = {mass: 0.1, rad: 0.05, refl: 0.7, mu_s: 0.4, mu_k: 0.2};
+const mPropB = {mass: 0.5, rad: 0.05, refl: 0.7, mu_s: 0.4, mu_k: 0.2};
+
+Builders.generateBox(1, 1, 1, 1, mPropA, 100, 50, ph);
+Builders.generateBox(2, 2, 1, 1, mPropA, 100, 50, ph);
+Builders.generateBox(3.5, 4.5, 1, 1, mPropB, 500, 250, ph);
+Builders.generateBox(0.05, 4.5, 1, 1, mPropB, 500, 250, ph);
+Builders.generateLine({x: 1, y: 1}, {x: 1.5, y: 1.5}, mPropA, 100, 50, ph);
+ph.mesh[16].fix = true;
+
+for (let i = 0; i < 20; i++) {
+    let m = new Mass(mPropA,
+        {x: Math.random() * env.boundary.w, y: Math.random() * env.boundary.h},
+        {x: Math.random() * env.boundary.w, y: Math.random() * env.boundary.h}
+    );
+    ph.addM(m);
+}
+
+const frame = (frameTime) => {
+    ctx.clearRect(0, 0, viewport.width, viewport.height);
+    userState.drawHighlight();
+    userState.drawSelect();
+    ph.drawSpring(ctx, '#000000');
+    ph.drawMass(ctx, '#1DB322');
+    if (!pause){
+        ph.update(ph.mesh.map(mass => {
+            let f = env.weight(mass).sum(env.drag(mass))
+            .sum(mass.springing()).sum(mass.damping())
+            f = f.sum(env.friction(mass, f));
+            return f;
+        }), delta);
+        ph.collision(ph.mesh.map(mass => env.boundaryHit(mass)))
+    }
+    ctx.fillStyle = "black";
+    ctx.fillText("Cursor: " + userState.mousePos.div(ph.scale).display(3), 20, 20);
+    ctx.fillText("Highlight: " + userState.highlight, 20, 40);
+    ctx.fillText("Select: " + userState.select, 20, 60);
+    ctx.fillText("Drag: " + userState.drag, 20, 80);
+    window.requestAnimationFrame(frame)
+}
+
+frame();
