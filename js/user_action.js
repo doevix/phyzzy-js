ce11ca9f8e263e204eec5f5d3522a0b5f72ffa52
Laura Fox <dannickfox@gmail.com>
2020-05-23 22:20:29 -0500

    menu distinction between masses and springs

--- js/user_action.js
+++ js/user_action.js
@@ -4,8 +4,26 @@
 const makeMenuContainer = () => {
     const div = document.createElement('div');
     div.className = 'menuContainer';
-    document.body.appendChild(div);
     return div;
+}
+
+const createSelectedMassMenu = m => {
+    const menu_div = document.createElement('div');
+    const p = document.createElement('p');
+    p.innerHTML = "Mass selected";
+    menu_div.appendChild(p);
+    const container = makeMenuContainer();
+    container.appendChild((menu_div));
+    return container;
+}
+const createSelectedSpringMenu = s => {
+    const menu_div = document.createElement('div');
+    const p = document.createElement('p');
+    p.innerHTML = "Spring selected";
+    menu_div.appendChild(p);
+    const container = makeMenuContainer();
+    container.appendChild((menu_div));
+    return container;
 }
 
 document.addEventListener('keypress', e =>  {
@@ -16,26 +34,31 @@ document.addEventListener('keypress', e =>  {
 const MouseHandler = (cv) => {
     const pos = new v2d();
     let selectMenu = undefined;
+    let selected = undefined;
     const listeners = () => {
         cv.addEventListener('mousemove', e => {
             pos.set(e.clientX - cv.offsetLeft + window.scrollX, e.clientY - cv.offsetTop + window.scrollY);
             Model.dragAction(e.movementX, e.movementY);
         }, false);
         cv.addEventListener('mousedown', e => {
-            const s = Model.setSelect(true);
-            if (s && e.which === 3) {
+            selected = Model.setSelect(true);
+            
+        }, false);
+        cv.addEventListener('mouseup', () => Model.clearDrag(), false);
+        cv.addEventListener('mouseleave', () => Model.clearDrag(), false);
+        cv.addEventListener('dblclick', e => {
+            if (selected) {
                 if (selectMenu !== undefined) {
                     document.body.removeChild(selectMenu);
                 }
-                selectMenu = makeMenuContainer();
-                selectMenu.innerHTML = JSON.stringify(s, null, '\t');
-            } else if (selectMenu !== undefined && e.which === 3) {
+                selectMenu = Mass.prototype.isPrototypeOf(selected) ?
+                    createSelectedMassMenu(selected) : createSelectedSpringMenu(selected);
+                document.body.appendChild(selectMenu);
+            } else if (selectMenu !== undefined) {
                 document.body.removeChild(selectMenu);
                 selectMenu = undefined;
             }
         }, false);
-        cv.addEventListener('mouseup', () => Model.clearDrag(), false);
-        cv.addEventListener('mouseleave', () => Model.clearDrag(), false);
         cv.addEventListener('contextmenu', e => e.preventDefault(), false);
     }
 
