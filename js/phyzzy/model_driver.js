c6dde8c8f88e889c786e02d238bb429414f152e9
Laura Fox <dannickfox@gmail.com>
2020-05-31 23:44:48 -0500

    mass-mass collisions

--- js/phyzzy/model_driver.js
+++ js/phyzzy/model_driver.js
@@ -90,6 +90,36 @@ class Mass {
         console.log(fk);
         this.F_sum.mAdd(fk);
     }
+    m_collide(masses, preserve) {
+        for (let i = 0; i < masses.length; ++i) {
+            const c = masses[i];
+            // Check groups.
+            if (this !== c && ((this.c_group === c.c_group && this.c_group !== 0 && c.c_group !== 0) || this.c_group === -1 || c.c_group === -1)) {
+                // Check overlap.
+                if (this.pos.isInRad(c.pos, this.radius + c.radius)) {
+                    const seg = this.pos.sub(c.pos);
+                    const D = seg.nrm().mul(this.radius + c.radius).sub(seg);
+
+                    const v_memA = {va: this.d_p(), vb: c.d_p()}
+                    const v_memB = {va: c.d_p(), vb: this.d_p()}
+                    
+                    if (!this.isFixed && !c.isFixed) {
+                        this.pos.mAdd(D.div(2));
+                        c.pos.mSub(D.div(2));
+                    } else if(this.isFixed && !c.isFixed) {
+                        c.pos.mSub(D);
+                    } else if(!this.isFixed && c.isFixed) {
+                        this.pos.mAdd(D);
+                    }
+
+                    if (preserve) {
+                        if (!this.isFixed) this.deflect(c, v_memA);
+                        if (!c.isFixed) c.deflect(this, v_memB);
+                    }
+                }
+            }
+        }
+    }
     // Reflect mass's velocity according to a tangent direction.
     reflect(tan) {
         this.set_d_p(this.d_p().pjt(tan).add(this.d_p().pjt(tan.prp()).inv().mul(this.refl)));
@@ -380,37 +410,6 @@ const Model = (() => {
     let drag = undefined;
     let pause = false;
 
-    // Mass-mass collisions.
-    const mm_collide = (m, preserve) => {
-        for (let i = 0; i < masses.length; ++i) {
-            const c = masses[i];
-            // Check groups.
-            if (m !== c && ((m.c_group === c.c_group && m.c_group !== 0 && c.c_group !== 0) || m.c_group === -1 || c.c_group === -1)) {
-                // Check overlap.
-                if (m.pos.isInRad(c.pos, m.radius + c.radius)) {
-                    const seg = m.pos.sub(c.pos);
-                    const D = seg.nrm().mul(m.radius + c.radius).sub(seg);
-
-                    const v_memA = {va: m.d_p(), vb: c.d_p()}
-                    const v_memB = {va: c.d_p(), vb: m.d_p()}
-                    
-                    if (!m.isFixed && !c.isFixed) {
-                        m.pos.mAdd(D.div(2));
-                        c.pos.mSub(D.div(2));
-                    } else if(m.isFixed && !c.isFixed) {
-                        c.pos.mSub(D);
-                    } else if(!m.isFixed && c.isFixed) {
-                        m.pos.mAdd(D);
-                    }
-
-                    if (preserve) {
-                        if (!m.isFixed) m.deflect(c, v_memA);
-                        if (!c.isFixed) c.deflect(m, v_memB);
-                    }
-                }
-            }
-        }
-    }
     // Mass-spring collisions.
     const ms_collide = m => {
         for (let i = 0; i < springs.length; ++i) {
@@ -445,11 +444,13 @@ const Model = (() => {
             }
         }
     }
-
+    
+    // Autoreverse function.
+    let autoRevEnable = true;
     let LR_prv = undefined;
     const toggleWaveDirection = () => dir = dir > 0 ? -1 : 1
     const autoReverse = LR => {
-        if (LR !== undefined && LR !== LR_prv) {
+        if (autoRevEnable && LR !== undefined && LR !== LR_prv) {
             LR_prv = LR;
             toggleWaveDirection();
         }
@@ -534,7 +535,8 @@ const Model = (() => {
                 if (collisions_enabled)
                 {
                     ms_collide(m);
-                    mm_collide(m, false);
+                    // mm_collide(m, false);
+                    m.m_collide(masses, false);
                 }
                 env.boundCollide(m, false);
                 const LR = env.s_boundHit(m, false);
@@ -545,7 +547,8 @@ const Model = (() => {
                 
                 // Collision deflections.
                 if (collisions_enabled)
-                    mm_collide(m, true);
+                    m.m_collide(masses, true);
+                    // mm_collide(m, true);
 
                 env.s_boundHit(m, true);
                 env.boundCollide(m, true);
