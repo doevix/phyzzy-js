256dc0a1bab28bf817456a6a057ba1cbe79f129f
Laura Fox <dannickfox@gmail.com>
2020-06-08 18:38:34 -0500

    inserted html&css

--- js/player.js
+++ js/player.js
@@ -1,15 +1,12 @@
 // player.js
 "use strict";
 
-// Generate the canvas element.
-const generateCanvas = theme => {
-    const canvas = document.createElement('canvas');
-    canvas.style = 'background-color: ' + theme.background;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    document.body.appendChild(canvas);
-    return canvas;
+const scaleCanvas = (cvs) => {
+    cvs.height = window.innerHeight - 50 - 20;
+    cvs.width = cvs.height * (16 / 9); // Adjust width to aspect ratio.
+    Model.setScale(cvs.width / 12);
 }
+
 // Updates and draws the model to the canvas.
 const CanvasModelUpdate = (ctx, env, theme, width, height) => {
     // Catch the element nearest to the pointer.
@@ -41,11 +38,14 @@ const theme_dark = {
 };
 
 // Model player initialization
-const canvas = generateCanvas(theme_dark);
+const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+scaleCanvas(canvas);
 // Attach mouse event handlers to the canvas.
 const mouse = MouseHandler(canvas, Model.getScale());
 mouse.attachEvents(canvas);
+// Resize canvas and scale when window changes size.
+window.onresize = () => scaleCanvas(canvas);
 
 
 const frame = () => {
